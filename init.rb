4c99e799bd77d87b3cf6934755131b57d39ed7fe
five above infinity <39030697+5inf@users.noreply.github.com>
2020-04-09 22:21:52 +0200

    Implement (static) svg annotations
    This change currently breaks max-width and max-height sizing.

--- init.rb
+++ init.rb
@@ -30,9 +30,12 @@ Redmine::Plugin.register :redmine_advancedimage do
         {{advimg(test.png,width=200px,height=400px,title=title)
 
         {{advimg(test.png,width=200px,height=400px,title=title)
-        arrow((50,50),90°,20,"red")
-        box((20,10),(30,40),"red",2)
-        text((50,90),"text","color"
+        arrow((10,10),90°,20,"red",2)
+        arrow((20,10),90°,20,"green",3)
+        arrow((30,10),90°,20,"blue",5)
+        box((50,20),(30,40),"red",2)
+        circle((50,50),20,"green",5)
+        text((10,55),"text","red")
         }}
 
         {{imglink(test.png)}}
@@ -50,14 +53,43 @@ macro :advimg do |obj, args, text|
           width = options[:width] || "50px"
           height = options[:height] || "50px"
 
-          img = image_tag(url_for(:controller => 'attachments', :id => attachment, :action => 'download'), :style => 'max-width:'+width+';max-height:'+height+';', :alt => attachment.filename)
+          imgurl = url_for(:controller => 'attachments', :id => attachment, :action => 'download')
+          #img = image_tag(imgurl, :style => 'max-width:'+width+';max-height:'+height+';', :alt => attachment.filename)
+
+          #TODO: calculate width and height accordingly to keep aspect ratio and also provide a correct size for the svg image
+
+          svgimg="<image href='"+imgurl+"' height='"+height+"' width='"+width+"'/>"
+          svgimg=svgimg.html_safe
+                if text.present?
+                        svgdefs = "<defs><marker id='head' orient='auto' markerWidth='4' markerHeight='6' refX='0.1' refY='2'> <path d='M0,0 V4 L2,2 Z' fill='red' /> </marker>  </defs> ".html_safe
+                        svgcircle = "<circle cx='50%' cy='50%' r='20%' stroke='green' stroke-width='5' fill='#00000000' />".html_safe
+                        svgrectangle="<rect x='50%' y='20%' width='30%' height='40%'  style='fill:blue;stroke:pink;stroke-width:5;fill-opacity:0.1;stroke-opacity:1.0' />".html_safe
+                        svgpath="<path marker-end='url(#head)' stroke-width='5' fill='none' stroke='black' d='M0,0 C45,45 45,-45 90,0' />".html_safe
+                        svgline1="<line marker-end='url(#head)' x1='10%' y1='10%' x2='10%' y2='30%' style='stroke:rgb(255,0,0);stroke-width:5' />".html_safe
+                        svgline2="<line marker-end='url(#head)' x1='20%' y1='10%' x2='20%' y2='30%' style='stroke:rgb(100,100,0);stroke-width:5' />".html_safe
+                        svgline3="<line marker-end='url(#head)' x1='30%' y1='10%' x2='30%' y2='30%' style='stroke:rgb(0,255,0);stroke-width:5' />".html_safe
+                        svgtext="<text x='10%' y='55%' fill='red'>Annotiation Text</text>".html_safe
+
+                        #svgimage = content_tag(:svg, safe_join([svgdefs, svgcircle, svgrectangle, svgpath, svgline, svgimg]), :width => width, :height => height)
+                        svgimage = content_tag(:svg, safe_join([svgdefs, svgimg, svgline1, svgline2, svgline3, svgrectangle, svgcircle, svgtext]), :width => width, :height => height)
+
+                else
+
+                        svgimage = content_tag(:svg, safe_join([svgimg]), :width => width, :height => height)
+
+                end
 
           out = ''.html_safe
-          link = link_to(img, url_for(:controller => 'attachments', :action => 'show', :id => attachment), :class => 'thumbnail', :title => title, :name => filename)
+          url= url_for(:controller => 'attachments', :action => 'show', :id => attachment)
+          link = link_to(svgimage, url, :class => 'thumbnail', :title => title, :name => filename)
           label = content_tag(:p, content_tag(:strong, 'Figure: '+title))
 
-          out << link
-          out << label
+          innerdivtop = content_tag(:div, link, :id => 'innertop'+divid)
+          innerdivbottom = content_tag(:div, label, :id => 'innerbottom'+divid)
+          outerdiv = content_tag(:div, safe_join([innerdivtop, ' ', innerdivbottom]), :id => 'outer'+divid)
+
+          out << outerdiv
+#         out << svgimage
           out
 
         else
@@ -81,3 +113,4 @@ macro :imglink do |obj, args|
 
   end
 end
+
