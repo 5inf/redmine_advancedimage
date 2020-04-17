14c54d1fa48ebe0965804c84989d07c8a093727f
5inf <39030697+5inf@users.noreply.github.com>
2020-04-17 13:07:27 +0200

    add border option for figure macro

--- init.rb
+++ init.rb
@@ -14,52 +14,74 @@ Redmine::Plugin.register :redmine_advancedimage do
 
     Redmine::WikiFormatting::Macros.register do
     desc <<-DESCRIPTION
-      Show an image with a subtitle and optionally add annotations to the image (not implemented yet)
-      Syntax:
-        {{advimg(FILENAME[,width=WIDTH,height=HEIGHT,title=TITLE)}}
-        FILENAME = the filename of the attached image
-        WIDTH = The maximum width of the image, aspect ratio is kept
-        HEIGHT = The maximum height of the image, aspect ratio is kept
-        TITLE = Title to display below the image. If empty the filename is used
+Show an image with a subtitle and optionally add annotations to the image (not implemented yet)
+Syntax:
+	{{figure(FILENAME[,label=LABEL,title=TITLE,width=WIDTH,height=HEIGHT,border=BORDER)
+	[ANNOTATIONS]
+	}}
+  FILENAME = the filename of the attached image
+	LABEL = Label under which the figure is referenced. FILENAME is used if no label is given
+  TITLE = Title to display below the image. If empty the LABEL or FILENAME is used
+	WIDTH = The maximum width of the image, aspect ratio is kept
+  HEIGHT = The maximum height of the image, aspect ratio is kept
+  BORDER = An optional border around the actual image to provide extra space for annotations
+	ANNOTATIONS = Annotations overlaying the picture. Supported are arrows, boxes, circles and text. See examples for details
 
-        {{imglink(FILENAME)}}
+  {{figurelink(LABEL,title=TITLE)}}
   
-      Examples:
-        {{advimg(test.png)}}
-      
-        {{advimg(test.png,width=200px,height=400px,title=title)
-          
-        {{advimg(test.png,width=200px,height=400px,title=title)
-        arrow((10,10),90°,20,"red",2)
-        arrow((20,10),90°,20,"green",3)
-        arrow((30,10),90°,20,"blue",5)
-        box((50,20),(30,40),"red",2)
-        circle((50,50),20,"green",5)
-        text((10,55),"text","red")
-        }}
+Examples:
+	{{figure(test.png)}}
 
-        {{imglink(test.png)}}
-  
-    DESCRIPTION
+	{{figure(test.png,width=200px,height=400px,title=title)
 
-    macro :image do |obj, args, text|
-      args, options = extract_macro_options(args, :width, :height, :title, :label)
+	{{figure(test.png,label=test,title=A test image,width=200px,height=400px,border=20)
+		arrow((10,10),90,20,"red",2)
+		arrow((20,10),90,20,"green",3)
+		arrow((30,10),90,20,"blue",5)
+		box((50,20),(30,40),"red",2)
+		circle((50,50),20,"green",5)
+		text((10,55),"text","red")
+	}}
+	
+	{{figurelink(test.png)}}
+DESCRIPTION
+
+    macro :figure do |obj, args, text|
+      args, options = extract_macro_options(args, :width, :height, :title, :label, :border)
       filename = args.first
       label = options[:label] || filename
+			border= options[:border].to_i || 0
       raise 'Filename required' unless filename.present?
   
-      regarrow = /arrow\(\s?\(\s?(\d+)\s?,\s?(\d+)\s?\)\s?,\s?(\d+)\s?,\s?(\d+)\s?,\s?"(\w+)"\s?,\s?(\d+)\s?\)/m
-      regcircle = /circle\(\s?\(\s?(\d+)\s?,\s?(\d+)\s?\)\s?,\s?(\d+)\s?,\s?"(\w+)"\s?,\s?(\d+)\s?\)/m
-      regtext = /text\(\s?\(\s?(\d+)\s?,\s?(\d+)\s?\)\s?,\s?"([\w\s]+)"\s?,\s?"(\w+)"\s?\)/m
-      regbox = /box\(\s?\(\s?(\d+)\s?,\s?(\d+)\s?\)\s?,\s?\(\s?(\d+)\s?,\s?(\d+)\s?\)\s?,\s?"(\w+)"\s?,\s?(\d+)\s?\)/m
+      regarrow = /arrow\(\s?\(\s?(-?\d+)\s?,\s?(-?\d+)\s?\)\s?,\s?(\d+)\s?,\s?(\d+)\s?,\s?"(\w+)"\s?,\s?(\d+)\s?\)/m
+      regcircle = /circle\(\s?\(\s?(-?\d+)\s?,\s?(-?\d+)\s?\)\s?,\s?(\d+)\s?,\s?"(\w+)"\s?,\s?(\d+)\s?\)/m
+      regtext = /text\(\s?\(\s?(-?\d+)\s?,\s?(-?\d+)\s?\)\s?,\s?"([\w\s]+)"\s?,\s?"(\w+)"\s?\)/m
+      regbox = /box\(\s?\(\s?(-?\d+)\s?,\s?(-?\d+)\s?\)\s?,\s?\(\s?(\d+)\s?,\s?(\d+)\s?\)\s?,\s?"(\w+)"\s?,\s?(\d+)\s?\)/m
       #https://regex101.com/
 
 
       if obj && obj.respond_to?(:attachments) && attachment = Attachment.latest_attach(obj.attachments, filename)
         title = options[:title] || attachment.title || filename
-        divid = "advimg" + SecureRandom.urlsafe_base64(8)
+        divid = "figure" + SecureRandom.urlsafe_base64(8)
         width = options[:width] || "50px"
         height = options[:height] || "50px"
+
+				widthvalue = 200
+				widthunit = "px"
+				width.scan(/(\d+)([\w%]{1,2})/) do |match|
+					widthvalue=match[0].to_i || 200
+					widthunit=match[1] || "px"
+				end
+				heightvalue= 200
+				heightunit ="px"
+				svgwidthvalue=widthvalue+widthvalue*2*border/100
+				height.scan(/(\d+)([\w%]{1,2})/) do |match|
+					heightvalue=match[0].to_i || 200
+					heightunit=match[1] || "px"
+				end
+				svgheightvalue=heightvalue+heightvalue*2*border/100
+				xoffset=(widthvalue*border/100)
+				yoffset=(heightvalue*border/100)
   
         imgurl = url_for(:controller => 'attachments', :id => attachment, :action => 'download')
         #img = image_tag(imgurl, :style => 'max-width:'+width+';max-height:'+height+';', :alt => attachment.filename)
@@ -68,8 +90,8 @@ Redmine::Plugin.register :redmine_advancedimage do
   
         out = ''.html_safe
 
-        svgimg="<image href='"+imgurl+"' height='"+height+"' width='"+width+"'/>"
-        svgimg=svgimg.html_safe
+        svginternalimage="<image href='"+imgurl+"' x='"+xoffset.to_s+widthunit+"', y='"+yoffset.to_s+heightunit+"', height='"+heightvalue.to_s+heightunit+"' width='"+widthvalue.to_s+widthunit+"'/>"
+        svginternalimage=svginternalimage.html_safe
       
         if text.present?
           #out << content_tag(:p, text)
@@ -84,67 +106,67 @@ Redmine::Plugin.register :redmine_advancedimage do
           lines.each do |line|
             line.scan(regarrow) do |match|
               #out << "arrow"+match.to_s
-              x2= match[0].to_s
-              y2= match[1].to_s
+              x2=(widthvalue*(match[0].to_i+border)/100).to_s
+              y2=(heightvalue*(match[1].to_i+border)/100).to_s
               angle=match[2].to_s
               length=match[3].to_s
               color=match[4].to_s
               strokeWidth=match[5].to_s
-              x1=(x2.to_i-length.to_i*Math::sin(angle.to_i*Math::PI/180)).to_s
-              y1=(y2.to_i-length.to_i*Math::cos(angle.to_i*Math::PI/180)).to_s
+              x1=(x2.to_i-length.to_i*heightvalue/100*Math::sin(angle.to_i*Math::PI/180)).to_s
+              y1=(y2.to_i-length.to_i*widthvalue/100*Math::cos(angle.to_i*Math::PI/180)).to_s
               headid="headid" + SecureRandom.urlsafe_base64(8)
               svgdef = "<defs><marker id='"+headid+"' orient='auto' markerWidth='4' markerHeight='6' refX='0.1' refY='2'> <path d='M0,0 V4 L2,2 Z' fill='"+color+"'/> </marker>  </defs> "
               svgdefs.push(svgdef.html_safe)
-              svgline= "<line marker-end='url(#"+headid+")' x1='"+x1+"%' y1='"+y1+"%' x2='"+x2+"%' y2='"+y2+"%' style='stroke:"+color+";stroke-width:"+strokeWidth+";fill-opacity:0.1;stroke-opacity:1.0' />"
+              svgline= "<line marker-end='url(#"+headid+")' x1='"+x1+widthunit+"' y1='"+y1+heightunit+"' x2='"+x2+widthunit+"' y2='"+y2+heightunit+"' style='stroke:"+color+";stroke-width:"+strokeWidth+";fill-opacity:0.1;stroke-opacity:1.0' />"
               svglines.push(svgline.html_safe)
             end
 
             line.scan(regtext) do |match|
               #out << "text"+match.to_s
-              x1= match[0].to_s
-              y1= match[1].to_s
+              x1= (widthvalue*(match[0].to_i+border)/100).to_s
+              y1= (heightvalue*(match[1].to_i+border)/100).to_s
               innertext=match[2].to_s
               color=match[3].to_s
-              svgtext="<text x='"+x1+"%' y='"+y1+"%' fill='"+color+"'>"+innertext+"</text>"
+              svgtext="<text x='"+x1+widthunit+"' y='"+y1+heightunit+"' fill='"+color+"'>"+innertext+"</text>"
               svgtexts.push(svgtext.html_safe)
             end
 
             line.scan(regbox) do |match|
               #out << "box"+match.to_s
-              x1= match[0].to_s
-              y1= match[1].to_s
-              x2= match[2].to_s
-              y2= match[3].to_s
+              x1= (widthvalue*(match[0].to_i+border)/100).to_s
+              y1= (heightvalue*(match[1].to_i+border)/100).to_s
+              x2= (widthvalue*(match[2].to_i+border)/100).to_s
+              y2= (heightvalue*(match[3].to_i+border)/100).to_s
               rectwidth=(x1.to_i-x2.to_i).abs.to_s
               rectheight=(y1.to_i-y2.to_i).abs.to_s
               color=match[4].to_s
               strokeWidth=match[5].to_s
-              svgrectangle="<rect x='"+x1+"%' y='"+y1+"%' width='"+rectwidth+"%' height='"+rectheight+"%' style='fill:blue;stroke:pink;stroke-width:5;fill-opacity:0.1;stroke-opacity:1.0' />"
+              svgrectangle="<rect x='"+x1+widthunit+"' y='"+y1+heightunit+"' width='"+rectwidth+widthunit+"' height='"+rectheight+heightunit+"' style='fill:blue;stroke:pink;stroke-width:5;fill-opacity:0.1;stroke-opacity:1.0' />"
               svgrectangles.push(svgrectangle.html_safe)
             end 
 
             line.scan(regcircle) do |match|
               #out << "circle"+match.to_s
-              x1= match[0].to_s
-              y1= match[1].to_s
+              x1= (widthvalue*(match[0].to_i+border)/100).to_s
+              y1= (heightvalue*(match[1].to_i+border)/100).to_s
               radius=match[2].to_s
               color=match[3].to_s
               strokeWidth=match[4].to_s
-              svgcircle = "<circle cx='"+x1+"%' cy='"+y1+"%' r='"+radius+"%' stroke='"+color+"' stroke-width='"+strokeWidth+"' fill='#00000000' />"
+              svgcircle = "<circle cx='"+x1+widthunit+"' cy='"+y1+heightunit+"' r='"+radius+"%' stroke='"+color+"' stroke-width='"+strokeWidth+"' fill='#00000000' />"
               svgcircles.push(svgcircle.html_safe)
             end
           end
 
-          svgimage = content_tag(:svg, safe_join([svgdefs, svgimg, svglines, svgrectangles, svgcircles, svgtexts]), :width => width, :height => height)
+          svgimage = content_tag(:svg, safe_join([svgdefs, svginternalimage, svglines, svgrectangles, svgcircles, svgtexts]), :width => svgwidthvalue.to_s+widthunit, :height => svgheightvalue.to_s+heightunit)
         else
-          svgimage = content_tag(:svg, safe_join([svgimg]), :width => width, :height => height)
+          svgimage = content_tag(:svg, safe_join([svginternalimage]), :width => svgwidthvalue.to_s+widthunit, :height => svgheightvalue.to_s+heightunit)
         end
     
         url= url_for(:controller => 'attachments', :action => 'show', :id => attachment)
         link = link_to(svgimage, url, :class => 'thumbnail', :title => title, :name => filename, :target => '_blank')
-        labeltag = content_tag(:p, content_tag(:strong, 'Figure ('+filename+'): '+title))
-        innerdivtop = content_tag(:div, link, :id => 'innertop'+divid)
-        innerdivbottom = content_tag(:div, labeltag, :id => 'figure.'+label)
+        labeltag = content_tag(:p, content_tag(:strong, 'Abbildung ('+filename+'|'+label+'): '+title))
+        innerdivtop = content_tag(:div, link, :id => 'figure.'+label)
+        innerdivbottom = content_tag(:div, labeltag, :id => 'innerbottom.'+divid)
         outerdiv = content_tag(:div, safe_join([innerdivtop, ' ', innerdivbottom]), :id => 'outer'+divid)
         out << outerdiv
 
@@ -155,22 +177,23 @@ Redmine::Plugin.register :redmine_advancedimage do
       end
     end
 
-    macro :imagelink do |obj, args|
-      args, options = extract_macro_options(args, :label)
-      filename = args.first || options[:label]
-      raise 'Filename or lable required' unless filename.present?
+    macro :figurelink do |obj, args|
+      args, options = extract_macro_options(args, :title)
+      label = args.first
+      title = options[:title] || args.first
+      raise 'label argument required' unless label.present?
 
       out = ''.html_safe
       #out << content_tag(:a,'Figure '+filename, :href => '#'+filename)
-      out << content_tag(:a, 'Abbildung '+filename, :href => '#figure.'+filename)
+      out << content_tag(:a, 'Abbildung '+title, :href => '#figure.'+label)
       out
     end
 
     macro :table do |obj, args, text|
-      args, options = extract_macro_options(args, :label, :title)
-      raise 'label argument required' unless options[:label].present?
-      label = options[:label] 
+      args, options = extract_macro_options(args, :title)
+      label = args.first
       title = options[:title] || label
+      raise 'label argument required' unless label.present?
       
 
       out = ""
@@ -195,20 +218,22 @@ Redmine::Plugin.register :redmine_advancedimage do
     end
 
     macro :tablelink do |obj, args|
-      args, options = extract_macro_options(args, :label)
-      raise 'label argument required' unless options[:label].present?
-      label = options[:label] 
+      args, options = extract_macro_options(args, :title)
+      label = args.first 
+			title = options[:title] || label
+      raise 'label argument required' unless label.present?
+
 
       out = ''.html_safe
-      out << content_tag(:a,'Tabelle '+label, :href => '#table.'+label)
+      out << content_tag(:a,'Tabelle '+title, :href => '#table.'+label)
       out
     end
 
     macro :formula do |obj, args, text|
-      args, options = extract_macro_options(args, :label, :title)
-      raise 'label argument required' unless options[:label].present?
-      label = options[:label] 
+      args, options = extract_macro_options(args, :title)
+      label = args.first 
       title = options[:title] || label
+      raise 'label argument required' unless label.present?
       
 
       out = ""
@@ -223,7 +248,7 @@ Redmine::Plugin.register :redmine_advancedimage do
 
         table = table.html_safe
 
-        labeldiv = content_tag(:p, content_tag(:strong, 'Formula ('+label+'): '+title))
+        labeldiv = content_tag(:p, content_tag(:strong, 'Formel ('+label+'): '+title))
         innerdivtop = content_tag(:div, labeldiv)
         innerdivbottom = content_tag(:div, table)
         outerdiv = content_tag(:div, safe_join([innerdivtop, ' ', innerdivbottom]), :id => 'formula.'+label)
@@ -233,9 +258,11 @@ Redmine::Plugin.register :redmine_advancedimage do
     end
 
     macro :formulalink do |obj, args|
-      args, options = extract_macro_options(args, :label)
-      raise 'label argument required' unless options[:label].present?
-      label = options[:label] 
+      args, options = extract_macro_options(args, :title)
+      label = args.first
+			title = options[:title] || label
+      raise 'label argument required' unless label.present?
+
 
       out = ''.html_safe
       out << content_tag(:a,'Formel '+label, :href => '#formula.'+label)
